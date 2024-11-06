87b962d6eb0761b701f52c40105049c8cb109ba4
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-11-06 14:43:10 -0300

    feat: add methods to TopicoService for filtering and listing topics with DTO conversion
    - Implemented listarTopicos to retrieve a paginated list of topics excluding those with EXCLUIDO status, with conversion to DadosListagemTopicosDTO.
    - Added listarTopicosPorCurso to filter topics by course ID, using pagination and conversion to DadosListagemTopicosDTO.
    - Created listarTopicosPorAno to fetch topics based on a specific year, accepting a BuscaAnoDTO for the year input, with paginated results converted to DadosListagemTopicosDTO.
    - Introduced listarDezTopicosPorDataCriacao to list the 10 oldest topics based on creation date, with direct conversion to a list of DadosListagemTopicosDTO.
    - Added convertePageDeTopicosParaDTO as a helper method to streamline Page<Topico> to Page<DadosListagemTopicosDTO> conversion.

--- src/main/java/org/FelipeBert/forum/infra/service/TopicoService.java
+++ src/main/java/org/FelipeBert/forum/infra/service/TopicoService.java
@@ -1,18 +1,23 @@
 package org.FelipeBert.forum.infra.service;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.FelipeBert.forum.domain.dto.in.BuscaAnoDTO;
 import org.FelipeBert.forum.domain.dto.in.CriarNovoTopicoDTO;
+import org.FelipeBert.forum.domain.dto.out.DadosListagemTopicosDTO;
 import org.FelipeBert.forum.domain.model.Status;
 import org.FelipeBert.forum.domain.model.Topico;
 import org.FelipeBert.forum.domain.validacoes.ValidadorCriacaoTopico;
 import org.FelipeBert.forum.infra.repository.CursoRepository;
 import org.FelipeBert.forum.infra.repository.TopicoRepository;
 import org.FelipeBert.forum.infra.repository.UsuarioRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TopicoService {
@@ -49,5 +54,39 @@ public class TopicoService {
         novoTopico.setStatus(Status.NAORESPONDIDO);
 
         repository.save(novoTopico);
+    }
+
+    public Page<DadosListagemTopicosDTO> listarTopicos(Pageable paginacao) {
+        var dados = repository.findByStatusNot(Status.EXCLUIDO, paginacao);
+
+        Page<DadosListagemTopicosDTO> topicos = convertePageDeTopicosParaDTO(dados);
+
+        return topicos;
+    }
+
+    public Page<DadosListagemTopicosDTO> listarTopicosPorCurso(Long id, Pageable paginacao) {
+        var dados = repository.findTopicosByCursoId(id, paginacao);
+
+        Page<DadosListagemTopicosDTO> topicos = convertePageDeTopicosParaDTO(dados);
+
+        return topicos;
+    }
+
+    public Page<DadosListagemTopicosDTO> listarTopicosPorAno(BuscaAnoDTO dado, Pageable paginacao) {
+        var dados = repository.findTopicosByYear(dado.ano(), paginacao);
+
+        Page<DadosListagemTopicosDTO> topicos = convertePageDeTopicosParaDTO(dados);
+
+        return topicos;
+    }
+
+    public List<DadosListagemTopicosDTO> listarDezTopicosPorDataCriacao(Pageable paginacao) {
+        var dados = repository.findTop10ByOrderByDataCriacaoAsc(paginacao);
+
+        return dados.stream().map(DadosListagemTopicosDTO::new).collect(Collectors.toList());
+    }
+
+    private Page<DadosListagemTopicosDTO> convertePageDeTopicosParaDTO(Page<Topico> dados){
+        return dados.map(DadosListagemTopicosDTO::new);
     }
 }
