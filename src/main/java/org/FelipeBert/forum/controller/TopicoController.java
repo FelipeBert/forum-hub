6761da4f3d4dc49bdce7ccca53895b3cd1ac2fc7
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-11-06 14:44:22 -0300

    feat: add endpoints in TopicoController for listing and filtering topics
    - Added listarTopicos endpoint to retrieve a paginated list of all topics, sorted by title, excluding those with EXCLUIDO status. Returns data as Page<DadosListagemTopicosDTO>.
    - Implemented listarTopicosPorCurso endpoint to list topics by course ID, with pagination and sorting by title, returning a Page<DadosListagemTopicosDTO>.
    - Created listarTopicosPorAno endpoint to filter topics by a specific year, accepting a BuscaAnoDTO object for the year input, with results paginated and sorted by title.
    - Added listarTopicosPorCriacao endpoint to fetch the 10 oldest topics based on creation date, with results as a list of DadosListagemTopicosDTO and sorted by title.

--- src/main/java/org/FelipeBert/forum/controller/TopicoController.java
+++ src/main/java/org/FelipeBert/forum/controller/TopicoController.java
@@ -1,13 +1,18 @@
 package org.FelipeBert.forum.controller;
 
 import jakarta.validation.Valid;
+import org.FelipeBert.forum.domain.dto.in.BuscaAnoDTO;
 import org.FelipeBert.forum.domain.dto.in.CriarNovoTopicoDTO;
+import org.FelipeBert.forum.domain.dto.out.DadosListagemTopicosDTO;
 import org.FelipeBert.forum.infra.service.TopicoService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/topicos")
@@ -24,5 +29,34 @@ public class TopicoController {
         topicoService.criarNovoTopico(dados);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DadosListagemTopicosDTO>> listarTopicos(@PageableDefault(size = 10, sort = {"titulo"}) Pageable paginacao){
+        var topicos = topicoService.listarTopicos(paginacao);
+
+        return ResponseEntity.ok(topicos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Page<DadosListagemTopicosDTO>> listarTopicosPorCurso(@PathVariable Long id, @PageableDefault(size = 10, sort = {"titulo"}) Pageable paginacao){
+        var topicos = topicoService.listarTopicosPorCurso(id, paginacao);
+
+        return ResponseEntity.ok(topicos);
+    }
+
+    @GetMapping("/ano")
+    public ResponseEntity<Page<DadosListagemTopicosDTO>> listarTopicosPorAno(@RequestBody @Valid BuscaAnoDTO ano,
+                                                                             @PageableDefault(size = 10, sort = {"titulo"}) Pageable paginacao){
+        var topicos = topicoService.listarTopicosPorAno(ano, paginacao);
+
+        return ResponseEntity.ok(topicos);
+    }
+
+    @GetMapping("/criacao")
+    public ResponseEntity<List<DadosListagemTopicosDTO>> listarTopicosPorCriacao(@PageableDefault(size = 10, sort = {"titulo"}) Pageable paginacao){
+        var topicos = topicoService.listarDezTopicosPorDataCriacao(paginacao);
+
+        return ResponseEntity.ok(topicos);
     }
 }
