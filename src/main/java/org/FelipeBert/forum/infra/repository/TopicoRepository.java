ec848708b3f188aa0163aadc8a1335d44ab8f877
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-11-06 14:41:55 -0300

    feat: add custom queries to TopicoRepository for filtering topics
    - Added findByStatusNot to retrieve topics excluding a specified status, supporting pagination.
    - Implemented findTopicosByCursoId to fetch topics by cursoId using a custom query with pagination support.
    - Added findTopicosByYear to filter topics by the year of creation, allowing dynamic year-based filtering with pagination.
    - Introduced findTop10ByOrderByDataCriacaoAsc to return the 10 oldest topics based on creation date, supporting pagination.

--- src/main/java/org/FelipeBert/forum/infra/repository/TopicoRepository.java
+++ src/main/java/org/FelipeBert/forum/infra/repository/TopicoRepository.java
@@ -1,8 +1,26 @@
 package org.FelipeBert.forum.infra.repository;
 
+import org.FelipeBert.forum.domain.model.Status;
 import org.FelipeBert.forum.domain.model.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
     boolean existsByTituloAndMensagem(String titulo, String mensagem);
+
+    Page<Topico> findByStatusNot(Status status, Pageable paginacao);
+
+    @Query("SELECT t FROM Topico t WHERE t.curso.id = :cursoId")
+    Page<Topico> findTopicosByCursoId(@Param("cursoId") Long cursoId, Pageable paginacao);
+
+    @Query("SELECT t FROM Topico t WHERE YEAR(t.dataCriacao) = :year")
+    Page<Topico> findTopicosByYear(@Param("year") int year, Pageable pagincao);
+
+    @Query("SELECT t FROM Topico t ORDER BY t.dataCriacao ASC")
+    List<Topico> findTop10ByOrderByDataCriacaoAsc(Pageable pageable);
 }
