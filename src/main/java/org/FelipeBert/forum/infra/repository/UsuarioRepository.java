ee4abd8d67a86c4932ed54063024a7cf71163a1e
Felipe Bertulino <117488841+FelipeBert@users.noreply.github.com>
2024-11-10 14:14:25 -0300

    feat: add methods to UsuarioRepository for user retrieval and email existence check
    - findByEmail(String email): Retrieves user details by email, used for authentication and user management.
    - existsByEmail(String email): Checks if a user exists by email, useful for validating unique email addresses during registration.

--- src/main/java/org/FelipeBert/forum/infra/repository/UsuarioRepository.java
+++ src/main/java/org/FelipeBert/forum/infra/repository/UsuarioRepository.java
@@ -2,6 +2,10 @@ package org.FelipeBert.forum.infra.repository;
 
 import org.FelipeBert.forum.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 }
